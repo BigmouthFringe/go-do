9319a64e3c9bbedacdf5f339546ac308b4b90de6
BigmouthFringe <bigmouthfringe@gmail.com>
2019-05-06 14:45:38 +0700

    Add task command

--- src/taskcmd/taskcmd.go
+++ src/taskcmd/taskcmd.go
@@ -2,13 +2,62 @@ package taskcmd
 
 import (
 	"../inphdlr"
-	"../task"
+	"../taskdmn"
+	"fmt"
 )
 
-func Execute(task *task.Task, args *inphdlr.Args) {
+func Execute(task taskdmn.Task, args *inphdlr.Args) {
+	// NOTE: This is exceptional case, because there's always need to be a root task
+	if task == nil {
+		panic("nil root task")
+	}
 	if args.Add != "" {
-
+		add(task, args)
+	}
+	if args.Remove != 0 {
+		remove(task, args)
+	}
+	if args.List == true {
+		fmt.Println(task)
 	}
 }
 
-func create(args *inphdlr.Args) *task.Task {}
+func add(task taskdmn.Task, args *inphdlr.Args) {
+	if args.Parent != "" {
+		parent := find(task, args.Parent)
+		if parent == nil {
+			return
+		}
+		if err := parent.AddTask(taskdmn.NewTask(args.Add, args.Composite)); err != nil {
+			fmt.Println(err)
+		}
+	} else {
+		if err := task.AddTask(taskdmn.NewTask(args.Add, args.Composite)); err != nil {
+			fmt.Println(err)
+		}
+	}
+}
+func remove(task taskdmn.Task, args *inphdlr.Args) {
+	if args.Parent != "" {
+		parent := find(task, args.Parent)
+		if parent == nil {
+			return
+		}
+		if err := parent.RemoveTask(args.Remove - 1); err != nil {
+			fmt.Println(err)
+		}
+	} else {
+		if err := task.RemoveTask(args.Remove - 1); err != nil {
+			fmt.Println(err)
+		}
+	}
+}
+
+func find(task taskdmn.Task, name string) taskdmn.Task {
+	foundTask, err := task.Find(name)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	return foundTask
+}
